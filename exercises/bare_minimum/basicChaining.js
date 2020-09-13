d238e01311763e9784e2ec41be5c4f9d7208a8b6
twzkraus <twzkraus@gmail.com>
2020-09-13 11:50:19 -0700

    Assignment Complete--bare minimum requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,41 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(readFilePath, (e1, content) => {
+      if (e1) {
+        reject(e1);
+      } else {
+        // console.log(content.toString().slice(0, content.toString().indexOf('\n')));
+        resolve(content.toString().slice(0, content.toString().indexOf('\n')));
+      }
+    });
+  }).then( (username) => {
+    return new Promise((resolve, reject) => {
+      request.get(`https://api.github.com/users/${username}`, (e2, response, body) => {
+        // console.log(body);
+        // SOMETHING IS FAILING HERE--WE CAN CONSOLE.LOG BODY, BUT IT WON'T PASS INTO NEXT FUNC
+        // IDEA FROM https://javascript.info/promise-chaining: make new promise inside of then. This is probably going to fix it.
+        resolve(body);
+      });
+    });
+  }).then((githubOutput) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(writeFilePath, githubOutput, (e3) => {
+        if (e3) {
+          reject(e3);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
 };
 
 // Export these functions so we can test them
